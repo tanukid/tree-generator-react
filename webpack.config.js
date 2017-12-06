2dc9d7d94306120735cca6a18007c4e278d45186
danielbh <danielhollcraft@gmail.com>
2017-12-06 09:33:07 -0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: './bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, './')
+        ],
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
