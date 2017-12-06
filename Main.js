91c9af30391814852a40fedc705e8b96eb81f96b
danielbh <danielhollcraft@gmail.com>
2017-12-06 09:43:02 -0500

    Make tree

--- Main.js
+++ Main.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import Tree from 'react-d3-tree'
+
+const myTreeData = [
+  {
+    name: 'Top Level',
+    children: [
+      {
+        name: 'Level 2: A',
+      },
+      {
+        name: 'Level 2: B',
+        children: [
+          {
+            name: 'Level 2: A',
+          },
+          {
+            name: 'Level 2: B',
+            children: [
+              {
+                name: 'Level 2: A',
+              },
+              {
+                name: 'Level 2: B',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+]
 
 const Main = () => (
-  <div>
-    test
+  <div id="treeWrapper" style={{ width: '50em', height: '20em' }}>
+    <Tree data={myTreeData} />
   </div>
 )
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
         include: [
           path.resolve(__dirname, './')
         ],
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        exclude: /node_modules/
       }
     ]
   }
